49d53d971a729052bf40bfb4487b87f9646e55f5
Horacio Daniel Ros <auxtral.os@gmail.com>
2026-06-04 20:57:48 -0300

    refactor: rename wf-utils and wf-touch libraries to meteor-utils and meteor-touch in debian packaging

--- build-deb.sh
+++ build-deb.sh
@@ -10,8 +10,8 @@ mv ../meteor_*.deb build-packages/ 2>/dev/null || true
 mv ../meteor-*.deb build-packages/ 2>/dev/null || true
 mv ../meteor_*.buildinfo build-packages/ 2>/dev/null || true
 mv ../meteor_*.changes build-packages/ 2>/dev/null || true
-mv ../libwf-*.deb build-packages/ 2>/dev/null || true
-mv ../libwf-*_*.buildinfo build-packages/ 2>/dev/null || true
-mv ../libwf-*_*.changes build-packages/ 2>/dev/null || true
+mv ../libmeteor-*.deb build-packages/ 2>/dev/null || true
+mv ../libmeteor-*_*.buildinfo build-packages/ 2>/dev/null || true
+mv ../libmeteor-*_*.changes build-packages/ 2>/dev/null || true
 
 echo "¡Listo! Todos los paquetes se encuentran en build-packages/"
